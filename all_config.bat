c57d7b0b5384aff80bd92f6b18c64f0ef96efc24
Alexey Ka <trafficreg@gmail.com>
2016-04-09 18:13:46 +0300

    Modifying build script all.bat

--- all_config.bat
+++ all_config.bat
@@ -5,19 +5,24 @@ Echo "Building configuration %1"
 ""C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"" x86
 
   msbuild src\utm\utm.vcxproj /p:Configuration=%1 |tee -a build.%1
-  if not !ERRORLEVEL!==0 goto fail
+  SET BUILDSTATUS=%ERRORLEVEL%
+  if NOT %BUILDSTATUS% == 0 goto fail
   copy /y src\%1\*.lib lib
   msbuild src\proxyserver\proxyserver.vcxproj /p:Configuration=%1 |tee -a build.%1
-  if not !ERRORLEVEL!==0 goto fail
+  SET BUILDSTATUS=%ERRORLEVEL%
+  if NOT %BUILDSTATUS% == 0 goto fail
   copy /y src\%1\*.lib lib
   msbuild src\corefilters\corefilters.vcxproj /p:Configuration=%1 |tee -a build.%1
-  if not !ERRORLEVEL!==0 goto fail
+  SET BUILDSTATUS=%ERRORLEVEL%
+  if NOT %BUILDSTATUS% == 0 goto fail
   copy /y src\%1\*.lib lib
   msbuild src\monitor\monitor.vcxproj /p:Configuration=%1 |tee -a build.%1
-  if not !ERRORLEVEL!==0 goto fail
+  SET BUILDSTATUS=%ERRORLEVEL%
+  if NOT %BUILDSTATUS% == 0 goto fail
   copy /y src\%1\*.lib lib
   msbuild src\dualserver\dualserver.vcxproj /p:Configuration=%1 |tee -a build.%1
-  if not !ERRORLEVEL!==0 goto fail
+  SET BUILDSTATUS=%ERRORLEVEL%
+  if NOT %BUILDSTATUS% == 0 goto fail
   copy /y src\%1\*.lib lib
 
 echo 0 > done.%1
